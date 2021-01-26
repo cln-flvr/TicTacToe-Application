caf53c6107c33f980124968716bc84123035f9bf
cln-flvr <celineflavier@gmail.com>
2021-01-26 18:39:37 +0800

    Added Condition for CheckWinner
    mResults[9]

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -65,15 +65,30 @@ namespace TicTacToe
             button.Content = mPlayer1Turn ? "x" : "o";
             if (!mPlayer1Turn)
                 button.Foreground = Brushes.Green;
-            mPlayer1Turn ^= true; // Flipping Values from true to false and vice versa
-
+                mPlayer1Turn ^= true; // Flipping Values from true to false and vice versa
+            Console.WriteLine("Check Winner");
             CheckWinner();
+            
         }
 
         private void CheckWinner()
         {
             // var same = mResults[0] && mResults[1] && mResults[2] == mResults[0];
             if (mResults[0] != MarkType.Free && (mResults[0] & mResults[1] & mResults[2]) == mResults[0])
+                mGameEnded = true;
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[3] & mResults[6]) == mResults[0])
+                mGameEnded = true;
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[4] & mResults[8]) == mResults[0])
+                mGameEnded = true;
+            if (mResults[1] != MarkType.Free && (mResults[1] & mResults[4] & mResults[7]) == mResults[1])
+                mGameEnded = true;
+            if (mResults[2] != MarkType.Free && (mResults[2] & mResults[5] & mResults[8]) == mResults[2])
+                mGameEnded = true;
+            if (mResults[3] != MarkType.Free && (mResults[3] & mResults[4] & mResults[5]) == mResults[3])
+                mGameEnded = true;
+            if (mResults[6] != MarkType.Free && (mResults[6] & mResults[7] & mResults[8]) == mResults[6])
+                mGameEnded = true;
+            if (mResults[6] != MarkType.Free && (mResults[6] & mResults[4] & mResults[2]) == mResults[6])
                 mGameEnded = true;
 
         }
